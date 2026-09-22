d65c6d5f5cfb46682f268f411938f167c2320928
Sean Shih <sean.shih.2005@gmail.com>
2026-09-22 13:23:10 +0800

    Connected to backend

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -7,41 +7,20 @@ interface PlayerCardProps {
 
  export default function PlayerCard({ player }: PlayerCardProps) {
 
-    if (player.position == "P") {
-        return (
-            <Link to={`player/${player.id}`}>
-                <div className='w-full max-w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl border'>
-                {/* <a href='#'> */}
-                    <img src='src/assets/player_placeholder.jpg' alt='Player Name (placeholder)' className='h-80 w-72 object-cover rounded-t-xl'></img>
-                    <div className='px-4 py-3 w-72'>
-                        <span className='text-gray-500 mr-3 uppercase text-xs'>{player.team}</span>
-                        <p className='text-lg font-bold text-black truncate block capitalize'>{player.name}</p>
-                        <div className='flex items-center'>
-                            <p className='text-base text-gray-400 justify-end-safe'>{player.era}</p>
-                        </div>
+    return (
+        <Link to={`player/${player.id}`}>
+            <div className='w-full max-w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl border'>
+                <img src='src/assets/player_placeholder.jpg' alt='Player Name (placeholder)' className='h-80 w-72 object-cover rounded-t-xl'></img>
+                <div className='px-4 py-3 w-72'>
+                    <span className='text-gray-500 mr-3 uppercase text-xs'>{player.team}</span>
+                    <p className='text-lg font-bold text-black truncate block capitalize'>{player.name}</p>
+                    <div className='flex items-center'>
+                        {/* <p className='text-base text-gray-400 justify-end-safe'>{player.era}</p> */}
+                        <p className='text-base text-gray-400 justify-end-safe'>{player.position === "P" ? player.era ?? "N/A" : player.battingAvg ?? "N/A"}</p>
                     </div>
-                {/* </a> */}
                 </div>
-            </Link>
-            
-        );
-    } else {
-        return (
-            <Link to={`player/${player.id}`}>
-                <div className='w-full max-w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl border'>
-                    {/* <a href='#'> */}
-                        <img src='src/assets/player_placeholder.jpg' alt='Player Name (placeholder)' className='h-80 w-72 object-cover rounded-t-xl'></img>
-                        <div className='px-4 py-3 w-72'>
-                            <span className='text-gray-500 mr-3 uppercase text-xs'>{player.team}</span>
-                            <p className='text-lg font-bold text-black truncate block capitalize'>{player.name}</p>
-                            <div className='flex items-center'>
-                                <p className='text-base text-gray-400'>{player.battingAvg}</p>
-                            </div>
-                        </div>
-                    {/* </a> */}
-                </div>
-            </Link>
-        );
-    }
-    
+            </div>
+        </Link>
+        
+    );    
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import { players } from '../data/players';
+import { useEffect, useState } from 'react'
+// import { players } from '../data/players';
 import SearchBar from '../components/SearchBar';
 import PlayerGrid from '../components/PlayerGrid';
+import type { Player } from '../types/player';
+import { data } from 'react-router-dom';
 
 
 function Home() {
     const [searchTerm, setSearchTerm] = useState("");
+    const [players, setPlayers] = useState<Player[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/players")
+            .then((response) => response.json())
+            .then((data) => {
+                setPlayers(data);
+        });
+    }, []);
+        
     
     const filteredPlayers = players.filter((player) => 
     player.name.toLowerCase().includes(searchTerm.toLowerCase()) || player.team.toLowerCase().includes(searchTerm.toLowerCase())
